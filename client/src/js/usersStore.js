8dad4d0c985c68569fffe74cd4ccc88a75e85739
unknown <Hyourinmaru80@gmail.com>
2018-11-14 09:24:41 +0200

    worked on store

---
+++ client/src/js/usersStore.js
@@ -0,0 +1,21 @@
+import { observable } from "mobx";
+
+class UsersStore {
+    @observable currentUser = {}
+
+    signUp = () => {
+
+    }
+
+    validate = () => {
+
+    }
+
+    logIn = () => {
+
+    }
+}
+
+const usersStore = new UsersStore();
+
+export default usersStore;
