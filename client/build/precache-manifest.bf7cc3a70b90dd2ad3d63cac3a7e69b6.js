d8d865099e6dc8c0e9abf63f6713776776bbe259
d-zaloziecki <pnides20@gmail.com>
2018-11-21 12:20:39 +0200

    build not ignored

---
+++ client/build/precache-manifest.bf7cc3a70b90dd2ad3d63cac3a7e69b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdd46d5c7f0d2b321cf1f05d6635fe3a",
+    "url": "/static/media/risestarhand.semibold.fdd46d5c.ttf"
+  },
+  {
+    "revision": "07e1c896b14cd2fa06911eab83ef39c2",
+    "url": "/static/media/floral-capitals.regular.07e1c896.ttf"
+  },
+  {
+    "revision": "7c5c02ae4922b0cff2b3e7a983d13be1",
+    "url": "/static/media/crawley.regular.7c5c02ae.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6da0111da13dcc09cdf7",
+    "url": "/static/js/main.6da0111d.chunk.js"
+  },
+  {
+    "revision": "b115d3b2545f7dbee715",
+    "url": "/static/js/1.b115d3b2.chunk.js"
+  },
+  {
+    "revision": "6da0111da13dcc09cdf7",
+    "url": "/static/css/main.3c3664cf.chunk.css"
+  },
+  {
+    "revision": "c494d643c6bd291863e8154914d3be86",
+    "url": "/index.html"
+  }
+];
